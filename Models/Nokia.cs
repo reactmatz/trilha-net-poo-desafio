a41487294cfe1f598da550673ba2c17cba06c99b
reactmatz <matheusnasc.contato@gmail.com>
2024-09-30 23:51:49 -0300

    feat: implementar classes derivadas Iphone e Nokia com método de instalação de aplicativos

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,8 +1,18 @@
 namespace DesafioPOO.Models
 {
-    // TODO: Herdar da classe "Smartphone"
-    public class Nokia
+    // Herdando da classe "Smartphone"
+    public class Nokia : Smartphone
     {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+        // Construtor passando todos os parâmetros para a classe base
+        public Nokia(string numero, string modelo, string imei, int memoria) 
+            : base(numero, modelo, imei, memoria)
+        {
+        }
+
+        // Sobrescrevendo o método "InstalarAplicativo"
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando o aplicativo {nomeApp} no Nokia.");
+        }
     }
 }
